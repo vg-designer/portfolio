8b58884340f7950d5345333b83792993c3b90439
evelin-code <f.gongoraandrade@gmail.com>
2024-08-25 18:43:41 -0500

    Add images

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -3,6 +3,11 @@ import './css/Projects.css';
 
 const projects = [
   {
+    image: '/imgs/logo jewel essence - 2024.webp',
+    title: 'Product Title 1',
+    description: 'This is a description of the product 3.',
+  },
+  {
     image: '/imgs/logo bellota - 2024.webp',
     title: 'Product Title 1',
     description: 'This is a description of the product 3.',
@@ -13,21 +18,30 @@ const projects = [
     description: 'This is a description of the product 3.',
   },
   {
-    image: '/imgs/logo vg - 2023.webp',
-    title: 'Product Title 2',
-    description: 'This is a description of the product 2.',
-  },
-  {
     image: '/imgs/logo hillary - 2024.webp',
     title: 'Product Title 4',
     description: 'This is a description of the product 3.',
   },
   {
-    image: '/imgs/logo jewel essence - 2024.webp',
+    image: '/imgs/logo juli market - 2024.webp',
     title: 'Product Title 5',
     description: 'This is a description of the product 3.',
   },
-  // Agrega más proyectos según sea necesario
+  {
+    image: '/imgs/logo office oasis - 2024.webp',
+    title: 'Product Title 5',
+    description: 'This is a description of the product 3.',
+  },
+  {
+    image: '/imgs/logo soulful silhouettes - 2024.webp',
+    title: 'Product Title 5',
+    description: 'This is a description of the product 3.',
+  },
+  {
+    image: '/imgs/logo turware - 2023.webp',
+    title: 'Product Title 1',
+    description: 'This is a description of the product 3.',
+  },
 ];
 
 export function Projects () {
